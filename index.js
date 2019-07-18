1cd67a2fffb25560c1389b15b6fffaf71993e45b
spiderpiggy03 <49546430+spiderpiggy03@users.noreply.github.com>
2019-07-18 05:32:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ let death = false;
 let previous_current = Math.round((new Date()).getTime() / 1000);
 let current_time = Math.round((new Date()).getTime() / 1000);
 
-let enemyIDs;
-let characterIDs;
+let last = false;
+
+getJSON();
 
 function loadJSON(url, callback) {
     const obj = new XMLHttpRequest();
@@ -25,15 +26,36 @@ function loadJSON(url, callback) {
 
     obj.send(null);
 }
+let enemyIDs;
+let characterIDs;
+let crownIDs;
+let weaponIDs;
+let mutationIDs;
 
-loadJSON( 'JSON/enemy.json', data => enemyIDs = JSON.parse(data) );
-loadJSON( 'JSON/character.json', data => characterIDs = JSON.parse(data) );
+async function getJSON() {
+    let response;
 
-function redirect() {
+    response = await fetch( 'JSON/enemy.json');
+    enemyIDs = await response.json();
+
+    response = await fetch( 'JSON/character.json');
+    characterIDs = await response.json();
+
+    response = await fetch( 'JSON/crown.json');
+    crownIDs = await response.json();
+
+    response = await fetch( 'JSON/weapon.json');
+    weaponIDs = await response.json();
+
+    response = await fetch( 'JSON/mutation.json');
+    mutationIDs = await response.json();
+}
+
+function redirect(file) {
     id = document.getElementById('steamID64').value;
     key = document.getElementById('streamKey').value;
 
-    window.location  = `tracker.php?steamID64=${id}&streamKey=${key}`;
+    window.location  = `${file}?steamID64=${id}&streamKey=${key}`;
 }
 
 function credentials(steamID64, streamKey) {
@@ -48,60 +70,73 @@ async function getData() {
     const response = await fetch(URL);
     data = await response.json();
 
-    if (data.current) {
-
-        display_current();
-    } else {
+    if (!data.current) {
         death = true;
         previous_current = current_time;
-        display_currentNothing();
     }
 
-    if (data.previous) {
-        // console.log(current - data.previous.timestamp);
-       //console.log(current.getTime() - start.getTime());
-
-        display_last();
-    } else {
-        display_lastNothing();
+    if (document.getElementsByClassName('current')[0]) {
+        display_current();
     }
+    display_last();
 
 }
 
 function display_current() {
 
-    let start = data.current.timestamp;
+    let current_world = "";
+    let current_worldLevel = "";
+    let current_loop = "";
+    let current_level = "";
+    let current_kills = "";
+    let current_type = "";
+    let minutes = "";
+    let seconds = "";
 
-    let timeDifference = current_time - start;
-    let minutes = Math.floor(timeDifference / 60);
-    let seconds = timeDifference % 60;
+    if (data.current) {
 
-    if ((seconds == 1 || seconds == 0) && minutes == 0 && !death) {
-        previous_current = current_time;
+        let start = data.current.timestamp;
+
+        let timeDifference = current_time - start;
+        let minutes = Math.floor(timeDifference / 60);
+        let seconds = timeDifference % 60;
+
+        if ((seconds == 1 || seconds == 0) && minutes == 0 && !death) {
+            previous_current = current_time;
+        } else {
+            death = false;
+        }
+
+        current_time = Math.round((new Date()).getTime() / 1000);
+
+        start = data.current.timestamp;
+
+        timeDifference = current_time - start;
+        minutes = Math.floor(timeDifference / 60);
+        seconds = timeDifference % 60;
+
+         current_world = data.current.world;
+         current_worldLevel = data.current.level;
+         current_loop = data.current.loops;
+         current_level = data.current.charlvl;
+         current_kills = data.current.kills;
+         current_type = data.current.type;
+
+        const currentTimeElement = document.getElementById("current-time");
+        currentTimeElement.textContent = `Time: ${minutes} : ${seconds}`;
+
+        const currentWorldElement = document.getElementById("current-world");
+        currentWorldElement.textContent = `World: ${current_world} - ${current_worldLevel}`;
     } else {
-        death = false;
+
+        const currentTimeElement = document.getElementById("current-time");
+        currentTimeElement.textContent = `Time: `;
+
+        const currentWorldElement = document.getElementById("current-world");
+        currentWorldElement.textContent = `World: `;
     }
 
-    current_time = Math.round((new Date()).getTime() / 1000);
 
-    start = data.current.timestamp;
-
-    timeDifference = current_time - start;
-    minutes = Math.floor(timeDifference / 60);
-    seconds = timeDifference % 60;
-
-    const current_world = data.current.world;
-    const current_worldLevel = data.current.level;
-    const current_loop = data.current.loops;
-    const current_level = data.current.charlvl;
-    const current_kills = data.current.kills;
-    const current_type = data.current.type;
-
-    const currentTimeElement = document.getElementById("current-time");
-    currentTimeElement.textContent = `Time: ${minutes} : ${seconds}`;
-
-    const currentWorldElement = document.getElementById("current-world");
-    currentWorldElement.textContent = `World: ${current_world} - ${current_worldLevel}`;
 
     const currentLoopElement = document.getElementById("current-loop");
     currentLoopElement.textContent = `Loop: ${current_loop}`;
@@ -118,27 +153,113 @@ function display_current() {
 }
 
 function display_last() {
-    const start = data.previous.timestamp;
+
+    let last_world = "";
+    let last_worldLevel = "";
+    let last_loop  = "";
+    let last_level = "";
+    let last_kills = "";
+    let last_type  = "";
+    let enemyName  = "";
+    let charName   = "";
+    let crownName  = "";
+    let wepAName   = "";
+    let wepBName   = "";
+
+    if (data.previous) {
+
+        const start = data.previous.timestamp;
     const timeDifference = previous_current - start;
     const minutes = Math.floor(timeDifference / 60);
     const seconds = timeDifference % 60;
 
-    const last_world = data.previous.world;
-    const last_worldLevel = data.previous.level;
-    const last_loop = data.previous.loops;
-    const last_level = data.previous.charlvl;
-    const last_kills = data.previous.kills;
-    const last_type = data.previous.type;
+     last_world = data.previous.world;
+     last_worldLevel = data.previous.level;
+     last_loop = data.previous.loops;
+     last_level = data.previous.charlvl;
+     last_kills = data.previous.kills;
+     last_type = data.previous.type;
+
     const enemyID = data.previous.lasthit;
-    const enemyName = enemyIDs[enemyID]["Enemy name"];
+     enemyName = enemyIDs[enemyID]["Enemy name"];
     const charID = data.previous.char;
-    const charName = characterIDs[charID]["Character"];
+     charName = characterIDs[charID]["Character"];
+     const crownID = data.previous.crown;
+     crownName = crownIDs[crownID]["Crown"];
+     const wepAID = data.previous.wepA;
+     wepAName = weaponIDs[wepAID]["Gun name"];
+     const wepBID = data.previous.wepB;
+     wepBName = weaponIDs[wepBID]["Gun name"];
 
-    const lastTimeElement = document.getElementById("last-time");
-    lastTimeElement.textContent = `Time: ${minutes} : ${seconds}`;
+        const lastTimeElement = document.getElementById("last-time");
+        lastTimeElement.textContent = `Time: ${minutes} : ${seconds}`;
 
-    const lastWorldElement = document.getElementById("last-world");
-    lastWorldElement.textContent = `World: ${last_world} - ${last_worldLevel}`;
+        const lastWorldElement = document.getElementById("last-world");
+        lastWorldElement.textContent = `World: ${last_world} - ${last_worldLevel}`;
+
+if (last) {
+    const mutationElement =  document.getElementById('mutations');
+    while (mutationElement.hasChildNodes()) {
+        mutationElement.removeChild(mutationElement.firstChild);
+    }
+
+    const mutationText = document.createElement('h1');
+    mutationText.textContent = "Mutations: ";
+    mutationElement.appendChild(mutationText);
+
+    const mutationArr = data.previous.mutations.split("");
+    for (i = 0; i < mutationArr.length; i++) {
+        if (mutationArr[i] == 1) {
+            addMutation(i);
+        }
+    }
+    const lastCharImgElement = document.getElementById('char-img');
+    if (data.previous.skin === 0) {
+        lastCharImgElement.setAttribute('src', `image/character/${charName}.png`);
+    }  else {
+        lastCharImgElement.setAttribute('src', `image/character/${charName}B.png`);
+    }
+
+    const lastEnemyImgElement = document.getElementById('enemy-img');
+    lastEnemyImgElement.setAttribute('src', `image/enemy/${enemyName}.gif`);
+
+    if (wepAID !== 0) {
+        const lastWepAImgElement = document.getElementById('wepA-img');
+        lastWepAImgElement.setAttribute('src', `image/weapon/${wepAName}.png`);
+    }
+
+    if (wepBID !== 0) {
+        const lastWepBImgElement = document.getElementById('wepB-img');
+        lastWepBImgElement.setAttribute('src', `image/weapon/${wepBName}.png`);
+    }
+
+    const lastCrownBImgElement = document.getElementById('crown-img');
+    lastCrownBImgElement.setAttribute('src', `image/crown/${crownName}.png`);
+}
+    } else {
+        const lastTimeElement = document.getElementById("last-time");
+        lastTimeElement.textContent = `Time: `;
+
+        const lastWorldElement = document.getElementById("last-world");
+        lastWorldElement.textContent = `World: `;
+
+if (last) {
+    const lastCharImgElement = document.getElementById('char-img');
+    lastCharImgElement.setAttribute('src', ``);
+
+    const lastEnemyImgElement = document.getElementById('enemy-img');
+    lastEnemyImgElement.setAttribute('img', ``);
+
+    const lastWepAImgElement = document.getElementById('wepA-img');
+    lastWepAImgElement.setAttribute('src', ``);
+
+    const lastWepBImgElement = document.getElementById('wepB-img');
+    lastWepBImgElement.setAttribute('src', ``);
+
+    const lastCrownBImgElement = document.getElementById('crown-img');
+    lastCrownBImgElement.setAttribute('src', ``);
+}
+    }
 
     const lastLoopElement = document.getElementById("last-loop");
     lastLoopElement.textContent = `Loop: ${last_loop}`;
@@ -157,51 +278,27 @@ function display_last() {
 
     const lastEnemyElement = document.getElementById("enemy");
     lastEnemyElement.textContent = `${enemyName}`;
+
+    const lastCrownElement = document.getElementById("crown");
+    lastCrownElement.textContent = `${crownName}`;
+
+    const lastWepAElement = document.getElementById("wepA");
+    lastWepAElement.textContent = `${wepAName}`;
+
+    const lastWepBElement = document.getElementById("wepB");
+    lastWepBElement.textContent = `${wepBName}`;
 }
 
-function display_currentNothing() {
-    const currentTimeElement = document.getElementById("current-time");
-    currentTimeElement.textContent = `Time:`;
+function addMutation(index) {
+    const parent = document.getElementById('mutations');
 
-    const currentWorldElement = document.getElementById("current-world");
-    currentWorldElement.textContent = `World:`;
+    const text = mutationIDs[index]["Mutation"];
+    const childText = document.createElement('h1');
+    childText.classList.add('name');
+    childText.textContent = text;
+    const childImg = document.createElement('img');
+    childImg.setAttribute('src', `image/mutation/${text}.png`);
+    childImg.classList.add('name');
 
-    const currentLoopElement = document.getElementById("current-loop");
-    currentLoopElement.textContent = `Loop:`;
-
-    const currentLevelElement = document.getElementById("current-level");
-    currentLevelElement.textContent = `Level:`;
-
-    const currentKillsElement = document.getElementById("current-kills");
-    currentKillsElement.textContent = `Kills:`;
-
-    const currentTypeElement = document.getElementById("current-type");
-    currentTypeElement.textContent = `Type:`;
+    parent.appendChild(childImg);
 }
-
-function display_lastNothing() {
-    const lastTimeElement = document.getElementById("last-time");
-    lastTimeElement.textContent = `Time:`;
-
-    const lastWorldElement = document.getElementById("last-world");
-    lastWorldElement.textContent = `World:`;
-
-    const lastLoopElement = document.getElementById("last-loop");
-    lastLoopElement.textContent = `Loop:`;
-
-    const lastLevelElement = document.getElementById("last-level");
-    lastLevelElement.textContent = `Level:`;
-
-    const lastKillsElement = document.getElementById("last-kills");
-    lastKillsElement.textContent = `Kills:`;
-
-    const lastTypeElement = document.getElementById("last-type");
-    lastTypeElement.textContent = `Type:`;
-
-    const lastCharElement = document.getElementById("char");
-    lastCharElement.textContent = ``;
-
-    const lastEnemyElement = document.getElementById("enemy");
-    lastEnemyElement.textContent = ``;
-}
-
--- tracker-lastRun.php
+++ tracker-lastRun.php
@@ -3,14 +3,14 @@
 <head>
     <link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet">
     <link rel='shortcut icon' type='image/png' href='icon.png' />
-    <link rel="stylesheet" type="text/css" href="style.css" >
+    <link rel="stylesheet" type="text/css" href="style-lastRun.css" >
     <script src="index.js"> </script>
     <script>
         window.onload = () => {
           let bannerNode = document.querySelector('[alt="www.000webhost.com"]').parentNode.parentNode;
           bannerNode.parentNode.removeChild(bannerNode);
         }
-        last = false;
+        last = true;
     </script>
 </head>
 <body>
@@ -24,34 +24,44 @@
 ?>
 
 <div class="tracker">
-    <h1 class="title" style="margin-top: 50px;">Current Run: </h1>
-    <div class="current">
-        <h1 id="current-time">Time:</h1>
-        <h1 id="current-world">World:</h1>
-        <h1 id="current-loop">Loop:</h1>
-        <h1 id="current-level">Level:</h1>
-        <h1 id="current-kills">Kills:</h1>
-        <h1 id="current-type">Type:</h1>
-    </div>
 
-    <h1 class="title" style="margin-top: 100px;"> Last Run: </h1>
+    <h1 class="title" style=""> Last Run </h1>
 
     <div class="last">
         <h1 id="last-time">Time:</h1>
-        <h1 id="last-world">World:</h1>
+        <h1 id="last-world"> World:</h1>
         <h1 id="last-loop">Loop:</h1>
         <h1 id="last-level">Level:</h1>
         <h1 id="last-kills">Kills:</h1>
-        <h1 id="last-type">Type:</h1>
-
-        <h1 id="last-char">Character:
-            <br>
+        <h1 id="last-type">Type:</h1>as
+           <br>
+            <img id="char-img" class="image" src=""/>
             <h1 id="char" class="name"></h1>
         </h1>
 
         <h1 id="last-enemy">Death:
+             <img id="enemy-img" class="image"  src=""/>
              <h1 id="enemy" class="name"></h1>
         </h1>
+
+        <h1 id="last-crown">Crown:
+            <img id="crown-img" class="image" style="width: 75px" src=""/>
+            <h1 id="crown" class="name"></h1>
+        </h1>
+
+        <h1 id="last-weapon">Weapons:
+            <img id="wepA-img" class="image-item" src=""/>
+            <h1 id="wepA" class="name"></h1>
+
+            <img id="wepB-img" class="image-item" src=""/>
+            <h1 id="wepB" class="name"></h1>
+        </h1>
+
+        <div id="mutations">
+
+        <h1 id="last-mutation">Mutations: </h1>
+
+        </div>
 
     </div>
 </div>
